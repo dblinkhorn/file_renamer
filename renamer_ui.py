22688f1366a152b6bed680dfd23709b9fdaf41c4
Doug Blinkhorn <dblinkhorn@gmail.com>
2023-01-05 14:38:29 -0800

    finished force case section and widget alignments

--- renamer_ui.py
+++ renamer_ui.py
@@ -14,8 +14,8 @@ def select_folder():
 
 def add_replacement():
     replacement_keys.append(StringVar())
-    replacement_entry = Entry(mainframe).grid(
-        column=0, row=len(replacement_keys)+1)
+    replacement_entry = ttk.Entry(mainframe).grid(
+        column=0, row=len(replacement_keys)+6)
 
 
 # these hold the Entry widgets that will comprise the key/value pairs for
@@ -30,14 +30,37 @@ root.title("File Renamer")
 mainframe = ttk.Frame(root, padding="12 12 12 12")
 mainframe.grid(column=0, row=0)
 
+# target folder
 target_path = StringVar()
-target_path_text = Text(mainframe, height=1, width=50).grid(column=0, row=0)
-
-add_replacement_btn = ttk.Button(
-    mainframe, text="Add", command=add_replacement).grid(column=0, row=1)
-
+target_path_text = ttk.Entry(
+    mainframe, textvariable=target_path, width=50)
+target_path_text.grid(column=0, row=1, padx=(0, 12))
+target_path_label = ttk.Label(
+    mainframe,
+    text='1. Select target folder:').grid(sticky=W, column=0, row=0)
 select_folder_btn = ttk.Button(
     mainframe, text="Select Folder",
-    command=select_folder).grid(column=1, row=0)
+    command=select_folder).grid(column=1, row=1)
+
+# force case
+case_value = StringVar()
+case_value.set(None)
+lowercase_check = Radiobutton(
+    mainframe, text=" Lowercase", variable=case_value,
+    value="lowercase").grid(column=0, row=3, sticky=W)
+lowercase_label = ttk.Label(mainframe, text="2. Force case (optional):").grid(
+    sticky=W, column=0, row=2, pady=(12, 0))
+uppercase_check = Radiobutton(
+    mainframe, text=" Uppercase", variable=case_value,
+    value="uppercase").grid(column=0, row=4, sticky=W)
+
+# replacement rules
+replacement_key_text = Text(mainframe, height=1, width=30)
+replacement_label = ttk.Label(
+    mainframe,
+    text="3. Add replacement rules:").grid(sticky=W, column=0, row=5, pady=12)
+
+# add_replacement_btn = ttk.Button(
+# mainframe, text="Add", command=add_replacement).grid(column=0, row=4)
 
 root.mainloop()
